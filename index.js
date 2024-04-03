b912d57475375e95de0fe33e7ece2c86d3685c54
Maxwel <maxwellbakora@gmail.com>
2024-04-03 13:54:54 +0300

    Finished this lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// function saturdayFun(toDo = 'swim') {
+//     if (toDo === toDo) {
+//       return `This Saturday, I want to ${toDo}!`;
+//     } else {
+//       return "This Saturday, I want to roller-skate!";
+//     }
+//   }
+function saturdayFun(toDo = 'roller-skate') {
+    return `This Saturday, I want to ${toDo}!`;
+  }
+
+  const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  function wrapAdjective(flair="*") {
+    return function(adjective="special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
